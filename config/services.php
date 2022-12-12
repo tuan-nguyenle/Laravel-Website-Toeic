7581e1530c3279196a69f972c0be7a12419e308f
tuannguyenle <nhabao706@gmail.com>
2022-12-12 19:25:01 +0700

    test gg login

--- config/services.php
+++ config/services.php
@@ -34,7 +34,8 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRECT'),
-        'redirect' => 'http://localhost/google/call-back',
+        // 'redirect' => 'http://localhost/google/call-back',
+        'redirect' => 'http://web-toeic-for-future.herokuapp.com/call-back',
     ],
 
     'facebook' => [
